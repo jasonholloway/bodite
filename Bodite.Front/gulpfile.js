22811ade23f29d42a0221aab87dc97765bd0944b
Jason Holloway <miceeatnicerice@hotmail.com>
2015-12-07 17:51:31 +0000

    Moving browserify into gulp...

--- Bodite.Front/gulpfile.js
+++ Bodite.Front/gulpfile.js
@@ -1,4 +1,10 @@
 var gulp = require('gulp');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+var debowerify = require('debowerify');
+var bulkify = require('bulkify');
+
+
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var del = require('del');
@@ -10,26 +16,7 @@ var print = require('gulp-print');
 var gulpif = require('gulp-if');
 
 var config = {
-    //JavaScript files that will be combined into a jquery bundle
-    scriptPaths: [
-        './bower_components/jquery/dist/jquery.js',
-        './bower_components/jquery-ui/jquery-ui.js',
-        './bower_components/jquery-validation/dist/jquery.validate.js',
-        './bower_components/jquery-validation-unobtrusive/jquery.validate.unobtrusive.js',
-        './bower_components/angular/angular.js',
-        './bower_components/angular-advanced-searchbox/dist/angular-advanced-searchbox.js',
-        './bower_components/angular-bootstrap/ui-bootstrap.js',
-        './bower_components/angular-route/angular-route.js',
-        './bower_components/blueimp-canvas-to-blob/js/canvas-to-blob.js',
-        './bower_components/pica/dist/pica.js',
-        './bower_components/aws-sdk-js/dist/aws-sdk.js',
-        './bower_components/smap/smap-shim.js',
-        './bower_components/fancytree/dist/jquery.fancytree.js',
-        './bower_components/fancytree/dist/src/jquery.fancytree.dnd.js',
-        './bower_components/jquery-contextmenu/contextmenu.js'
-    ],
-
-
+    
     cssPaths: [
         './bower_components/jquery-ui/themes/smoothness/jquery-ui.css',
         './bower_components/bootstrap/dist/css/bootstrap.css',
@@ -51,9 +38,13 @@ var config = {
 };
 
 gulp.task('scripts', [], function () {
-    return gulp.src(config.scriptPaths)
-                .pipe(gulp.dest('./Content/js'))
-                .pipe(print());
+    return browserify('./Scripts/bb-admin.js', { debug: true })
+            .transform(debowerify)
+            .transform(bulkify)
+            .bundle()
+            .pipe(source('bundle.js'))
+            .pipe(gulp.dest('./Content/js'))
+            .pipe(print());
 });
 
 
