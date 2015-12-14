3c887c23dd1fcf08af59a3babc172fc564fbcfd7
Jason Holloway <miceeatnicerice@hotmail.com>
2015-12-14 21:03:31 +0000

    Gulp tasks weren't returning streams

--- Bodite.Ng/gulpfile.js
+++ Bodite.Ng/gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var watch = require('gulp-watch');
+var merge = require('gulp-merge');
 var source = require('vinyl-source-stream');
 var browserify = require('browserify');
 var bulkify = require('bulkify');
@@ -20,81 +21,53 @@ gulp.task('default', ['html', 'js', 'css', 'images'], function() {
 
 
 gulp.task('html', function() {
-    gulp.src('./index.html')
-    .pipe(watch('./index.html'))
-    .pipe(gulp.dest('./build/'))
-    .pipe(print());
+    var index = gulp.src('index.html')
+                .pipe(watch('index.html'))
+                .pipe(gulp.dest('build/'))
+                .pipe(print());
     
-    gulp.src('./templates/*.html')
-    .pipe(watch('./templates/*.html'))
-    .pipe(gulp.dest('./build/templates/'))
-    .pipe(print());    
+    var templates = gulp.src('templates/**/*.html')
+                    .pipe(watch('templates/**/*.html'))
+                    .pipe(gulp.dest('build/templates/'))
+                    .pipe(print());
+                     
+    return merge(index, templates);   
 })
 
 
 gulp.task('css', function() {
-    gulp.src('./css/*.css')
-    .pipe(watch('./css/*.css'))
-    .pipe(gulp.dest('./build/css/'))
-    .pipe(print());
+    return gulp.src('css/**/*.css')
+            .pipe(watch('css/**/*.css'))
+            .pipe(gulp.dest('build/css/'))
+            .pipe(print());
 })
 
 gulp.task('images', function() {
-    gulp.src('./images/*.*')
-    .pipe(watch('./images/*.*'))
-    .pipe(gulp.dest('./build/images/'))
-    .pipe(print());
+    return gulp.src('images/**/*.*')
+            .pipe(watch('images/**/*.*'))
+            .pipe(gulp.dest('build/images/'))
+            .pipe(print());
 })
 
 
-gulp.task('js', function() {	
-        
-    var cacheFilePath = './temp/watchify.cache.json';
+gulp.task('js', function() {	        
+    var cacheFilePath = 'temp/watchify.cache.json';
         
     var b = browserify({ 
-                    entries: ['./js/app.js'], 
+                    entries: ['js/app.js'], 
                     cache: watchify.getCache(cacheFilePath), 
                     packageCache: {}, 
                     debug: true 
                 })
-                .transform(bulkify)
-                //.transform(babelify)
-                //.transform(uglifyify)
-                //.transform(function(f) {
-                //    return /^bb-/.test(f)    //NOT WORKING!!!!
-                //            ? babelify(f, { presets: ['es2015'] })
-                //            : new through(function() { });
-                //})
-                //.transform(function (f, o) {
-                //    return /^bb-/.test(f)
-                //            ? new through(function() { })
-                //            : uglifyify(f, o);
-                //})
-                .plugin(function (b) {                    
-//                     b.on('reset', attach);
-//                     attach();
-// 
-//                     function attach() {
-//                         b.pipeline.get('debug')
-//                             .push(through.obj(function (data, enc, cb) {
-//                                 data.sourceFile = path.relative(process.cwd(), data.sourceFile);
-//                                 data.sourceFile = path.join('http://localhost:9967', data.sourceFile);
-//                                 //data.sourceFile = data.sourceFile.replace(/\\/g, '/');
-//                                 cb(null, data);
-//                             }));
-//                     }
-                });
+                .transform(bulkify);
                 
     var w = watchify(b, { cacheFile: cacheFilePath });
                 
     function rebundle() {
         return w.bundle()
-                //  .pipe(mold.transform(function(sourceMap, cb) {
-                //      cb(sourceMap);
-                //  }))
-                .pipe(exorcist('./build/js/bundle.js.map'))
+                .pipe(exorcist('build/js/bundle.js.map', null, 'http://localhost:9967/'))
                 .pipe(source('bundle.js'))
-                .pipe(gulp.dest('./build/js'))
+                .pipe(gulp.dest('build/js'))
                 .pipe(print())
                 .on('end', function() {
                    w.write(); 
