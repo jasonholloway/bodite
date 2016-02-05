ebb9af676a8765a1f25398df20d95e3c2c75a45b
Jason Holloway <miceeatnicerice@hotmail.com>
2016-02-05 16:20:58 +0000

    Fixed bower

--- admin/test/ProductEditorTest.js
+++ admin/test/ProductEditorTest.js
@@ -1,6 +1,5 @@
 require('./_global');
 require('../js/directives/productEditor');
-// require('../js/services/machineNames');
 
 var expect = require('chai').expect;
 var sinon = require('sinon');
@@ -60,59 +59,6 @@ describe('ProductEditor', function() {
     });
              
         
-   
-    // it('fills machine name on leaving name field', function() {
-    //     var x = compileEditor();   
-        
-    //     var elName = x.elem.find('div.names input.LV');        
-    //     var elMachName = x.elem.find('div.machine-name input');
-                
-    //     elName.val('jason').trigger('input');
-    //     x.scope.$apply();
-                     
-    //     elName.blur();
-               
-    //     return new Promise(function(done) {         
-    //         process.nextTick(function() {                
-    //             x.scope.$apply();
-                                      
-    //             expect(x.scope.$$childHead.editor.working.machineName, 'model').to.equal('jason');
-    //             expect(elMachName.val(), 'view').to.equal('jason');
-                
-    //             done();            
-    //         });
-    //     })
-    // });
-   
-   
-    // it.skip('machine name is limited by mask', function() {
-    //     //Apparently needs protractor, cos Karma doesn't do integration testing, apparently...
-    //     //Though it surely bloody does. Can't sendkeys however without special interface to browser.
-        
-    //     var x = compileEditor();   
-        
-    //     var elMach = $(x.elem).find('div.machine-name input');
-        
-    //     elMach[0].clear().sendKeys('illegal name!');
-    //     x.scope.$apply();
-        
-    //     return new Promise(function(done) {
-    //         process.nextTick(function() {
-    //             expect(elMach.val()).to.be.empty;
-    //             done(); 
-    //         });            
-    //     });        
-    // });
-    
-    
-    // it.skip('manually editing machine name stops auto-creation', function(cb) {
-    //     //flag to be set on product        
-    //     //..
-        
-    //     //not entirely necessary at moment
-    //     //...
-    // });
-    
     
    
     
