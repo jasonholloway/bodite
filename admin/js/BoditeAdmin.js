8349f89b64d09015a6e0835a97215ccaa6eb016d
Jason Holloway <miceeatnicerice@hotmail.com>
2016-02-05 17:10:41 +0000

    Removed ngPatternRestrict

--- admin/js/BoditeAdmin.js
+++ admin/js/BoditeAdmin.js
@@ -4,10 +4,9 @@ require('angular-bootstrap');
 require('angular-sanitize');
 require('angular-route');
 require('angular-dialog-service');
-require('ng-pattern-restrict');
 var urlJoin = require('url-join');
 
-var app = angular.module('BoditeAdmin', ['ngRoute', 'ui.bootstrap', 'dialogs.main', 'ngPatternRestrict']);
+var app = angular.module('BoditeAdmin', ['ngRoute', 'ui.bootstrap', 'dialogs.main']);
 
 var dbBaseUrl = 'https://jasonholloway.cloudant.com/bb/';
 
--- admin/test/_global.js
+++ admin/test/_global.js
@@ -1,7 +1,6 @@
 window.$ = window.jQuery = require('jquery');
 require('angular');
 require('angular-mocks');
-require('ng-pattern-restrict');
 
-angular.module('BoditeAdmin', ['ngPatternRestrict']);
+angular.module('BoditeAdmin', []);
 
